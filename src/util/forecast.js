5907736450113f98abe00d946fbfa11924642263
Windows.A <andre.inacio.silveira@gmail.com>
2022-01-04 11:27:25 -0300

    Correção no forecast

--- src/util/forecast.js
+++ src/util/forecast.js
@@ -3,7 +3,7 @@ const chalk = require('chalk')
 const fs = require('fs')
 
 const forecast = (latitude,longitude, callback) => {
-    const url = "http://api.weatherstack.comweatherstack.com/current?access_key=39d889d4d94d647bf6fad173651c0701&query="+latitude+","+longitude 
+    const url = "http://api.weatherstack.com/current?access_key=39d889d4d94d647bf6fad173651c0701&query="+latitude+","+longitude 
     
     request({url, json: true},(error, response)=>{
         if(error){
@@ -14,11 +14,13 @@ const forecast = (latitude,longitude, callback) => {
             dados = {
                 descricao: response.body.current.weather_descriptions[0],
                 temperatura: response.body.current.temperature,
-                sensacao: response.body.current.feelslike
+                sensacao: response.body.current.feelslike,
+                umidade: response.body.current.humidity,
+                hora: response.body.location.localtime
             }
-            const {descricao,temperatura,sensacao} = dados
+            const {descricao,temperatura,sensacao,umidade, hora} = dados
             //fs.appendFileSync('log.txt',descricao +'. A temperatura atual é de '+temperatura+'ºC. A sensação térmica é de '+sensacao+'ºC.')
-            callback(undefined,'A temperatura atual é de '+ temperatura + 'ºC. A sensação térmica é de '+ sensacao +'ºC.')
+            callback(undefined,'Data/Hora: '+hora+'\n A temperatura atual é de '+ temperatura + 'ºC.\n A sensação térmica é de '+ sensacao +'ºC.\n A umidade é igual a ' + umidade + '%')
             //callback(undefined, dados)
             
         }      
